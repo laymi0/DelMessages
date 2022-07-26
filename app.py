da7e4bfbd850f6add7e29cd5c303295a60bc0af9
Азизбек <72821937+laymi0@users.noreply.github.com>
2022-07-26 11:20:41 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,17 @@
 from pyrogram import Client, filters
 from pyrogram.handlers import MessageHandler
 
-
 def delete(client, message):
 
-    
         if 'начала' in message.text:
 
             message.delete()
 
-
 def main():
     app = Client('bot', 123, '')
-
     
     app.add_handler(MessageHandler(delete), filters.bot)
     app.run()
-    
 
 if __name__ == '__main__':
     main()
